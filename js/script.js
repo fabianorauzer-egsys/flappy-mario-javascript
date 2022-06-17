731a533eb9b22d488f1cca2e109295eb8454f2b8
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-16 22:36:48 -0300

    Atualização de personagens

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const yoshi = document.querySelector('.yoshi');
+const castle = document.querySelector('.castle');
+const peach = document.querySelector('.peach');
 
 
 const jump = () => {
@@ -11,15 +15,36 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    
+    const cloudsPosition = clouds.offsetLeft;
+    const yoshiPosition = yoshi.offsetLeft;
+    const castlePosition = castle.offsetLeft;
+    const peachPosition = peach.offsetLeft;
+
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        yoshi.style.animation = 'none';
+        yoshi.style.left = `${yoshiPosition}px`;
+        yoshi.src = "./images/yoshi.png";
+
+        peach.style.animation = 'none';
+        peach.style.left = `${peachPosition}px`;
+        peach.src = './images/helpPeach.png';
+        peach.style.width = '120px';
+        peach.style.bottom = '-8px';
+
+        castle.style.animation = 'none';
+        castle.style.left = `${castlePosition}px`;
 
         mario.src = './images/game-over.png';
         mario.style.width = '65px';
